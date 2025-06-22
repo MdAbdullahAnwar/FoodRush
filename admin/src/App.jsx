dcff01bc80fe28c8ba645b544c8b056e5cfee5fc
MdAbdullahAnwar <abdullahanwar2109@gmail.com>
2025-06-23 03:54:54 +0530

    List Items Functionality Completed

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,7 +5,7 @@ import {Routes,Route} from 'react-router-dom'
 import Add from './pages/Add/Add'
 import List from './pages/List/List'
 import Orders from './pages/Orders/Orders'
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 
 const App = () => {
   return (
--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,12 +1,64 @@
-import React from 'react'
-import './List.css'
+import React, { useEffect, useState } from 'react';
+import './List.css';
+import { toast } from 'react-toastify';
+import { db } from '../../firebase';
+import { collection, getDocs, deleteDoc, doc } from 'firebase/firestore';
 
 const List = () => {
-  return (
-    <div>
-      list
-    </div>
-  )
-}
+  const [list, setList] = useState([]);
 
-export default List
+  const fetchList = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(db, 'foods'));
+      const items = querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setList(items);
+    } catch (error) {
+    //   console.error(error);
+      toast.error('Failed To Fetch Data');
+    }
+  };
+
+  const removeFood = async (foodId) => {
+    try {
+      await deleteDoc(doc(db, 'foods', foodId));
+      toast.success('Food Removed Successfully');
+      fetchList();
+    } catch (error) {
+    //   console.error(error);
+      toast.error('Failed To Delete Item');
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
+  return (
+    <div className="list add flex-col">
+      <p>All Foods List</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {list.map((item, index) => (
+          <div key={index} className="list-table-format">
+            <img src={item.image} alt={item.name} />
+            <p>{item.name}</p>
+            <p>{item.category}</p>
+            <p>${item.price}</p>
+            <p onClick={() => removeFood(item.id)} className="cursor">X</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default List;
