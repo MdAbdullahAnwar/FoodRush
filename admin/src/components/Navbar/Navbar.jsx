20a6c47301df76f7ad6816310d48e1a50246d6b6
MdAbdullahAnwar <abdullahanwar2109@gmail.com>
2025-06-26 06:22:21 +0530

    Auth Implemented for Admin using Clerk

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
-import './Navbar.css'
-import { assets } from '../../assets/assets'
+import React from 'react';
+import { UserButton, useUser } from '@clerk/clerk-react';
+import { assets } from '../../assets/assets';
+import { useNavigate } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
+  const { isSignedIn } = useUser();
+  const navigate = useNavigate();
+
   return (
     <div className='navbar'>
       <img className='logo' src={assets.logo} alt='FoodRush Admin'/>
-      <img className='profile' src={assets.profile_image} alt="😎"/>
+      <div className="navbar-profile">
+        <button className="home-btn" onClick={() => navigate('/')}>Home</button>
+        {isSignedIn ? (
+          <UserButton afterSignOutUrl="/" />
+        ) : (
+          <img className='profile' src={assets.profile_image} alt="Profile"/>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -1,27 +1,30 @@
-import React from 'react'
-import './Sidebar.css'
-import { assets } from '../../assets/assets'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import { SignedIn } from '@clerk/clerk-react';
+import { NavLink } from 'react-router-dom';
+import { assets } from '../../assets/assets';
+import './Sidebar.css';
 
 const Sidebar = () => {
   return (
-    <div className="sidebar">
-      <div className="sidebar-options">
-        <NavLink to='./add' className="sidebar-option">
-            <img src={assets.add_icon} alt="Add"/>
-            <p>Add Items</p>
-        </NavLink>
-        <NavLink to='./list' className="sidebar-option">
-            <img src={assets.order_icon} alt="List"/>
-            <p>List Items</p>
-        </NavLink>
-        <NavLink to='./orders' className="sidebar-option">
-            <img src={assets.order_icon} alt="Orders"/>
-            <p>Orders</p>
-        </NavLink>
+    <SignedIn>
+      <div className="sidebar">
+        <div className="sidebar-options">
+          <NavLink to='/add' className="sidebar-option">
+              <img src={assets.add_icon} alt="Add"/>
+              <p>Add Items</p>
+          </NavLink>
+          <NavLink to='/list' className="sidebar-option">
+              <img src={assets.order_icon} alt="List"/>
+              <p>List Items</p>
+          </NavLink>
+          <NavLink to='/orders' className="sidebar-option">
+              <img src={assets.order_icon} alt="Orders"/>
+              <p>Orders</p>
+          </NavLink>
+        </div>
       </div>
-    </div>
-  )
-}
+    </SignedIn>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
